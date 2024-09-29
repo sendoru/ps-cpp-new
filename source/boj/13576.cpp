6d0832dd0c52317a89d3e8853ef6b12bb953fcf7
sendoru <sendol39@gmail.com>
2024-09-29 23:11:38 +0900

    Update

--- source/boj/13576.cpp
+++ source/boj/13576.cpp
@@ -9,56 +9,62 @@ typedef pair<ll, ll> pll;
 const ll MOD9 = 998244353;
 const ll MOD1 = (ll)1e9 + 7;
 
-vector<int> get_sa(string &s)
+vector<int> get_suffix_array(string &s)
 {
     int n = s.size();
-    vector<int> sa(n);
-    vector<int> group(n + 1);
-    vector<int> new_group(n + 1);
-
+    vector<int> sa(n), group(n + 1), new_group(n + 1);
     for (int i = 0; i < n; i++) {
         sa[i] = i;
         group[i] = s[i];
     }
 
-    for (int diff = 1; diff < n; diff <<= 1) {
-        auto cmp = [&](int i, int j) {
-            if (group[i] == group[j]) {
-                return group[i + diff] < group[j + diff];
-            }
+    group[n] = -1;
+    auto cmp = [&](int i, int j) {
+        if (group[i] == group[j]) {
+            return group[i + 1] < group[j + 1];
+        }
+        else {
             return group[i] < group[j];
-        };
-        sort(sa.begin(), sa.end(), cmp);
+        }
+    };
 
-        new_group[sa[0]] = 1;
+    for (int d = 1; d < n; d <<= 1) {
+        sort(sa.begin(), sa.end(), cmp);
+        new_group[sa[0]] = 0;
+        new_group[n] = -1;
         for (int i = 1; i < n; i++) {
-            new_group[sa[i]] = new_group[sa[i - 1]] + cmp(sa[i - 1], sa[i]);
+            if (cmp(sa[i - 1], sa[i])) {
+                new_group[sa[i]] = new_group[sa[i - 1]] + 1;
+            }
+            else {
+                new_group[sa[i]] = new_group[sa[i - 1]];
+            }
         }
         group = new_group;
     }
+
     return sa;
 }
 
 vector<int> get_lcp_array(string &s, vector<int> &sa)
 {
     int n = s.size();
-    vector<int> lcp(n);
     vector<int> pos(n);
     for (int i = 0; i < n; i++) {
         pos[sa[i]] = i;
     }
 
-    for (int i = 0, k = 0; i < n; i++) {
+    vector<int> lcp(n, 0);
+    for (int i = 0, k = 0; i < n; i++, k = max(k - 1, 0)) {
         if (pos[i] == n - 1) {
             continue;
         }
+
         for (int j = sa[pos[i] + 1]; s[i + k] == s[j + k]; k++) {
         }
         lcp[pos[i]] = k;
-        if (k) {
-            k--;
-        }
     }
+
     return lcp;
 }
 
@@ -66,19 +72,18 @@ vector<int> get_z_array(string &s)
 {
     int n = s.size();
     vector<int> z(n);
-    for (int i = 1, l = 0, r = 0; i < n; i++) {
-        if (i <= r) {
-            z[i] = min(r - i + 1, z[i - l]);
+    for (int i = 1, left = 0, right = 0; i < n; i++) {
+        if (i <= right) {
+            z[i] = min(right - i + 1, z[i - left]);
         }
         while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
             z[i]++;
         }
-        if (i + z[i] - 1 > r) {
-            l = i;
-            r = i + z[i] - 1;
+        if (i + z[i] - 1 > right) {
+            left = i;
+            right = i + z[i] - 1;
         }
     }
-
     return z;
 }
 
@@ -90,35 +95,22 @@ int main()
 
     string s;
     cin >> s;
-
-    vector<int> sa = get_sa(s);
-    vector<int> sa_inv(s.size());
-    for (int i = 0; i < s.size(); i++) {
-        sa_inv[sa[i]] = i;
-    }
-    vector<int> lcp = get_lcp_array(s, sa);
-    vector<int> z = get_z_array(s);
-
-    int n = s.size();
-    // get all i s.t. s[i:] is prefix of s
-    // and get the number of occurance of s[i:] in s for each i
-    vector<int> idxs = {n};
-    vector<int> occurs = {1};
-    for (int i = 0; i < n; i++) {
-        if (z[i] == n - i) {
-            idxs.push_back(n - i);
-            int sa_idx = sa_inv[i];
-            // TODO fix
-            occurs.push_back(sa_inv[0] - sa_idx + 1);
+    auto z = get_z_array(s);
+    auto tmp = z;
+    tmp[0] = s.size();
+    sort(tmp.begin(), tmp.end());
+    vector<pii> ans;
+    for (int i = s.size() - 1; i >= 0; i--) {
+        if (z[i] == s.size() - i) {
+            ans.push_back({z[i], tmp.end() - lower_bound(tmp.begin(), tmp.end(), z[i])});
         }
     }
 
-    reverse(idxs.begin(), idxs.end());
-    reverse(occurs.begin(), occurs.end());
+    ans.push_back({s.size(), 1});
 
-    cout << idxs.size() << '\n';
-    for (int i = 0; i < idxs.size(); i++) {
-        cout << idxs[i] << ' ' << occurs[i] << '\n';
+    cout << ans.size() << '\n';
+    for (auto &p : ans) {
+        cout << p.first << ' ' << p.second << '\n';
     }
 
     return 0;
